07585e649b91676e33118808b4a5294e6a5ea01b
ScottyPoi <kyzambo@gmail.com>
2021-12-07 00:36:33 -0700

    bring up to date with portal network repo

--- utp/Protocol/utp_protocol.ts
+++ utp/Protocol/utp_protocol.ts
@@ -2,118 +2,64 @@ import { _UTPSocket } from "../Socket/_UTPSocket";
 import { bufferToPacket, ConnectionState, Packet, randUint16 } from "..";
 import { Discv5 } from "@chainsafe/discv5";
 import { debug } from "debug";
+import { PortalNetwork } from "../../..";
 
-const log = debug("<uTP>")
+const log = debug("<uTP>");
 
 export class UtpProtocol {
+  portal: PortalNetwork
   sockets: Record<string, _UTPSocket>;
-  client: Discv5
-  payloadChunks: Buffer[];
+  client: Discv5;
 
-  constructor(client: Discv5) {
-  this.client = client;
+  constructor(portal: PortalNetwork) {
+    this.portal = portal
+    this.client = portal.client;
     this.sockets = {};
-    this.payloadChunks = [];
-  }
-  
-
-  // TODO: Chop up CONTENT into chunks.
-  // TODO: Reassemble chunks
-
-  async processContent(payload: Buffer): Promise<void> {
-    let packetSize = 1200;
-    for (let i=0; i<payload.length; i+= packetSize) {
-      this.payloadChunks.push(payload.subarray(i, i+packetSize))
-    }
   }
 
-  
-  
-  
-  nextChunk(): Buffer {
-    return this.payloadChunks.pop() as Buffer
-  }
-  
-  
-  
-  async initiateSyn(dstId: string): Promise<number> {
-    log(`Requesting uTP stream connection with ${dstId}`)
-    const socket = new _UTPSocket(this.client)
+  async initiateConnectionRequest(dstId: string): Promise<number> {
+    log(`Requesting uTP stream connection with ${dstId}`);
+    const socket = new _UTPSocket(this, dstId);
     this.sockets[dstId] = socket;
-    
-    await this.sockets[dstId].sendSyn(dstId);
-    return this.sockets[dstId].sndConnectionId
-  }
-  
-  async handleSynAck(payload: Buffer, dstId: string, content: Buffer): Promise<void> {
-    log("Received ST_STATE packet...SYN acked...Connection established.")
-    const ack = bufferToPacket(payload);
-    await this.processContent(content)
-    this.sockets[dstId].state = ConnectionState.Connected;
-    this.sockets[dstId].ackNr = ack.header.seqNr;
-    this.payloadChunks.length > 0
-    ? await this.sendData(this.nextChunk(), dstId)
-    : await this.sockets[dstId].sendFin(dstId);
+
+    await this.sockets[dstId].sendSynPacket();
+    return this.sockets[dstId].sndConnectionId;
   }
 
-  async handleAck(payload: Buffer, dstId: string): Promise<void> {
-    log("Received ST_STATE packet from " + dstId)
-    const ack = bufferToPacket(payload);
-    this.sockets[dstId].state = ConnectionState.Connected;
-    this.sockets[dstId].ackNr = ack.header.seqNr;
-    this.payloadChunks.length > 0
-    ? await this.sendData(this.nextChunk(), dstId)
-    : await this.sockets[dstId].sendFin(dstId);
+  async sendData(data: Buffer, dstId: string): Promise<void> {
+    this.sockets[dstId].startDataTransfer(data);
   }
-  async handleFin(payload: Buffer, dstId: string): Promise<void> {
-    log("Received ST_FIN packet from " + dstId + "...uTP stream closing...")
-    const ack = bufferToPacket(payload);
-    this.sockets[dstId].state = ConnectionState.Destroy
-    this.sockets[dstId].ackNr = ack.header.seqNr;
+
+  async handleSynAck(ack: Packet, dstId: string): Promise<void> {
+    log("Received ST_STATE packet...SYN acked...Connection established.");
+    this.sockets[dstId].handleSynAckPacket(ack);
   }
-  
-  async sendData(chunk: Buffer, dstId: string): Promise<void> {
-    await this.sockets[dstId].sendData(
-      this.sockets[dstId].seqNr,
-      this.sockets[dstId].ackNr,
-      this.sockets[dstId].sndConnectionId,
-      chunk,
-      dstId
-      );
-    }
-    
-    async handleIncomingSyn(packetAsBuffer: Buffer, dstId: string): Promise<void> {
-      log(`Received incoming ST_SYN packet...uTP connection requested by ${dstId}`)
-      let socket = new _UTPSocket(this.client);
-      this.sockets[dstId] = socket;
-      const packet: Packet = bufferToPacket(packetAsBuffer)
-    this.sockets[dstId].updateRTT(packet.header.timestampDiff);
-    this.sockets[dstId].rcvConnectionId = packet.header.connectionId + 1;
-    this.sockets[dstId].sndConnectionId = packet.header.connectionId;
-    this.sockets[dstId].seqNr = randUint16();
-    this.sockets[dstId].ackNr = packet.header.seqNr;
-    this.sockets[dstId].state = ConnectionState.SynRecv;
-    await this.sockets[dstId].sendAck(
-      this.sockets[dstId].seqNr++,
-      this.sockets[dstId].sndConnectionId,
-      this.sockets[dstId].ackNr,
-      dstId
+
+  async handleAck(packet: Packet, dstId: string): Promise<void> {
+    log("Received ST_STATE packet from " + dstId);
+    this.sockets[dstId].handleStatePacket(packet);
+  }
+  async handleFin(packet: Packet, dstId: string): Promise<void> {
+    log("Received ST_FIN packet from " + dstId + "...uTP stream closing...");
+    this.sockets[dstId].handleFinPacket(packet);
+  }
+
+  async handleIncomingConnectionRequest(
+    packet: Packet,
+    dstId: string
+  ): Promise<void> {
+    log(
+      `Received incoming ST_SYN packet...uTP connection requested by ${dstId}`
     );
-    log(`uTP stream opened with ${dstId}`)
+    let socket = new _UTPSocket(this, dstId);
+    this.sockets[dstId] = socket;
+    await this.sockets[dstId].handleIncomingConnectionRequest(packet);
+    log(`uTP stream opened with ${dstId}`);
   }
 
   async handleIncomingData(packet: Packet, dstId: string): Promise<void> {
-    this.sockets[dstId].updateRTT(packet.header.timestampDiff);
-    this.sockets[dstId].ackNr = packet.header.seqNr;
-    this.sockets[dstId].state = ConnectionState.Connected;
-    await this.sockets[dstId].sendAck(
-      this.sockets[dstId].seqNr++,
-      this.sockets[dstId].ackNr,
-      this.sockets[dstId].sndConnectionId,
-      dstId
-    );
+    log(`Receiving Data Packet from ${dstId}`);
+    await this.sockets[dstId].handleDataPacket(packet);
+    log(`Received Data Packet from ${dstId}`);
   }
-
-
-
 }
